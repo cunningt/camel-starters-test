81ccd8b243a2364695533389076176e889be4320
Tom Cunningham <tcunning@redhat.com>
2022-05-12 22:29:44 -0400

    Add generator and allpossible test

--- generator/src/main/java/org/fuse/camel/springboot/TestCreator.java
+++ generator/src/main/java/org/fuse/camel/springboot/TestCreator.java
@@ -1,11 +1,16 @@
 package org.fuse.camel.springboot;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
-
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
 import org.jboss.shrinkwrap.resolver.api.maven.pom.ParsedPomFile;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.shrinkwrap.resolver.api.maven.MavenStrategyStage;
@@ -38,20 +43,46 @@ public class TestCreator
         return model;
     }
 
-    public static void main( String[] args )
+    public Model readModel(String pomFileName) throws Exception {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader(pomFileName));
+        return model;
+    }
+
+    public void writeDependencies(Model model, List<Dependency> dependencies, String version) throws Exception {
+        for (Dependency d : dependencies) {
+            model.addDependency(d);
+        }
+        model.setProperty("camel-spring-boot-version", version);
+    }
+
+    public void writeModel(Model model, String pomFileName) throws Exception {
+        MavenXpp3Writer writer = new MavenXpp3Writer();
+        writer.write(new FileWriter(pomFileName), model);
+    }
+
+    public static void main(String[] args)
     {
         // Assuming that we are taking in a version of 
         String version = args[0];
+        String templateFile = args[1];
+        String outputFile = args[2];
+
         String gav = getGAV(version);
         
         TestCreator tc = new TestCreator();
         try {
             Model model = tc.resolveModel(gav);
-            System.out.println(model.toString());
+
             List<Dependency> dependencies = model.getDependencyManagement().getDependencies();
             for (Dependency d : dependencies) {
                 System.out.println(d.getGroupId() + ":" + d.getArtifactId() + ":" + d.getVersion());
             }
+
+            Model templateModel = tc.readModel(templateFile);
+            tc.writeDependencies(templateModel, dependencies, version);
+            tc.writeModel(templateModel, outputFile);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
